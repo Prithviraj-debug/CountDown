5b5e62cd78d6da241b0df59a26da90ecd3a93860
ptIThvie <57887221+Prithviraj-debug@users.noreply.github.com>
2021-07-15 17:26:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-    const countDate = new Date("July 13, 2021 00:00:00").getTime();
+    const countDate = new Date("September 21, 2021 00:00:00").getTime();
     const now = new Date().getTime();
     const gap = countDate - now;
 
